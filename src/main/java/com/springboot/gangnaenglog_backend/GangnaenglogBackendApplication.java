808058475c7b742e2ae1689abd2fe5669ec325a6
parky <parkyw0424@naver.com>
2025-04-14 14:20:56 +0900

    파일 생성

---
+++ src/main/java/com/springboot/gangnaenglog_backend/GangnaenglogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.gangnaenglog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangnaenglogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GangnaenglogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/gangnaenglog_backend/GangnaenglogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gangnaenglog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangnaenglogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
